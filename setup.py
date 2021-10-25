c10b21e788be09b13025695cde6f258a0253e967
0ops <909070781@qq.com>
2021-10-25 23:13:16 +0800

    auntpy

--- setup.py
+++ setup.py
@@ -12,25 +12,24 @@ except ImportError:
 
 
 setup(
-    name='Autnpy',
-    version='0.2.2',#该信息是必不可少的
+    name='autnpy',
+    version='0.2.4',
     author='0opslab',
     author_email='909070781@qq.com',
     platforms='any',
     description='Autnpy universal function',
     license='MIT',
-    packages=['',],#需要安装的代码包，也可以用find_packages函数
-    # install_requires=['mako>=1.0.3',
-    #                   ],#一些第三方账号，需要在这里申明。
+    packages=['autnpy'],
+    # install_requires=['mako>=1.0.3',               ],
     classifiers = [
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        ],#这也需要遵循标准格式
+        ],
     keywords='Autnpy universal function',
     url='https://github.com/0opslab',
-    zip_safe=True,#设为True，以zip的方式进行传输
-    include_package_data=True,#字面意思就可理解
+    zip_safe=True,
+    include_package_data=True,
     
 )
